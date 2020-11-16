0cfcdbb62a5d01e6574d237a46cc28b56583ed87
Heath Lester <h.allen.lester@gmail.com>
2020-11-16 13:10:45 -0600

    added authentication IF statement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,18 @@ import { Nutshell } from "./Nutshell.js"
 
 
 /*
-    1. Check if the user is authenticated by looking in session storage for `activeUser`
-    2. If so, render the Nutshell component
-    3. If not, render the login and registration forms
-    4. Also, if the user authenticates, and the login form is initially shown
-        ensure that the Nutshell component gets rendered
+1. Check if the user is authenticated by looking in session storage for `activeUser`
+2. If so, render the Nutshell component
+3. If not, render the login and registration forms
+4. Also, if the user authenticates, and the login form is initially shown
+ensure that the Nutshell component gets rendered
 */
+
+
+debugger
+if (sessionStorage.activeUser === true) {
+    Nutshell()
+} else {
+    RegisterForm()
+    LoginForm()
+}
