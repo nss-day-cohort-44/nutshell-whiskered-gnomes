5481e3368b61b2772785f8203f697bcec672e38d
Kristen Steele <ksteelephoto@gmail.com>
2020-11-17 14:41:42 -0600

    add getMessages and useMessages functions and tested. getting expected results

--- src/scripts/messages/MessageDataProvider.js
+++ src/scripts/messages/MessageDataProvider.js
@@ -1,1 +1,22 @@
 // contains at least getMessages and useMessages
+let messages = []
+
+const eventHub = document.querySelector(".container")
+
+export const getMessages = () => {
+    return fetch("http://localhost:8088/messages?_expand=user")
+    .then(response => response.json())
+    .then(parsedMessages => {
+        // console.log(parsedMessages)
+        messages = parsedMessages
+    })
+}
+
+export const useMessages = () => {
+        const sortedByTimestamp = messages.sort(
+            (a, b) => {
+                return b.timestamp-a.timestamp}
+                )
+                console.log("sorted messages", sortedByTimestamp)
+                return sortedByTimestamp
+    }
