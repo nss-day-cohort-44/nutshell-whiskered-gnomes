eea70ca6a55160517fdb05d9f67f981163bea228
Kristen Steele <ksteelephoto@gmail.com>
2020-11-17 15:16:22 -0600

    add MessageList function to call get, use and render

--- src/scripts/messages/MessageList.js
+++ src/scripts/messages/MessageList.js
@@ -1,17 +1,32 @@
 // import messageHTML, getMessages, useMessages
+import { getMessages, useMessages } from "./MessageDataProvider.js"
 
 // define eventHub and contentTarget
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".messages")
 
 // should the variables needed be modular in scope?
+let messages = []
 
 /* MessageList will call getMessages, useMessages, and render.
 Should I limit the number of messages that come back in the array?
 Is this where they should be sorted by timestamp? */
+export const MessageList = () => {
+    getMessages()
+        .then(() => {
+            messages = useMessages()
+            console.log("MessageList messages: ", messages)
+            renderMessages()
+        })
+}
 
 /* render will define an empty string to hold html reps, 
 iterate through messages array and call messageHTML for each message obj,
 then append the html reps to the dom in ascending order. 
 Is this where ascending order should be implemented? */
+
+
+
 
 /* add event listener to listen for message dp state change event for when a message is 
 added or deleted? Does the whole list really need to re-render each time a message is added?
