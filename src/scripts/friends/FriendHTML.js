136cf5d2d6bbbbf0ad355a89e75378788088e46e
Heath Lester <h.allen.lester@gmail.com>
2020-11-17 15:16:34 -0600

    commit to review pull request

--- src/scripts/friends/FriendHTML.js
+++ src/scripts/friends/FriendHTML.js
@@ -8,8 +8,23 @@ export const friendHTML = friendObj => {
         <div class="friend__card" id=${friendObj.id}>
             <p class="friend__username">${friendObj.username}</p>
             <div class="friend__buttons">
-            <button id="delete--${friendObj.id}">Remove</button>
+            <button id="deleteFriend--${friendObj.id}">Remove</button>
             </div>
         </div>
     `
 }
+
+// Dispatches userId from input form
+eventHub.addEventListener("click", deleteClick => {
+    if (deleteClick.target.id.startsWith("deleteFriend--")) {
+        // debugger
+        const [prefix, friendId] = deleteClick.target.id.split("--")
+        // finish below
+        const saveEvent = new CustomEvent("addSavedFriend", {
+            detail: {
+                friendUsername: friendToSave
+            }
+        })
+        eventHub.dispatchEvent(saveEvent)
+    }
+})
