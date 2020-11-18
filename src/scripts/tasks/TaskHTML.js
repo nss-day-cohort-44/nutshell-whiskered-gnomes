7c7fa6d611d24f608f1756365bd7029477b417f1
Jasmin Kaset <kasetski@gmail.com>
2020-11-18 13:28:38 -0600

    changed radio button to checkbox

--- src/scripts/tasks/TaskHTML.js
+++ src/scripts/tasks/TaskHTML.js
@@ -5,7 +5,7 @@ export const Task = (taskObj) => {
     <div class="task--card" id="userId--${taskObj.userId}">
     <h3>${taskObj.task}</h3>
     <p>Expected Completion: ${taskObj.completeDate}</p>
-    <input type="radio" id="completeTask--${taskObj.id}" name="completed" value="completed" />
+    <input type="checkbox" id="completeTask--${taskObj.id}" name="completed" value="completed" />
     <label for="completed">Select when completed</label>
     <button id="deleteTask--${taskObj.id}">Delete</button>
   </div>
