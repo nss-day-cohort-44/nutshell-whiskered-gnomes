dc106e609b0640726ffcd4289dfa4ec39a3dc424
Kristen Steele <ksteelephoto@gmail.com>
2020-11-18 13:53:28 -0600

    add function to render message form to DOM

--- src/scripts/messages/MessageForm.js
+++ src/scripts/messages/MessageForm.js
@@ -1,7 +1,17 @@
+/* author: Kristen Steele | module purpose: renders a chat form to the dom, 
+then listens for the form button click, saves input values and uses them as 
+an argument to call the saveMessage function */
+
 /* MessageForm is called by Nutshell.
 This function defines a content target and adds to it the html to render a message form 
 for adding a new message to the public chat, consisting of an input field and a send button. */
-
+export const MessageForm = () => {
+    const contentTarget = document.querySelector(".message__form")
+    contentTarget.innerHTML = `
+    <textarea class="message__field" id="message__field" placeholder="type message here"></textarea>
+    <button id="saveMessage">send</button>
+    `
+}
 
 /* This page needs an event listener to listen for the click of the send button,
 gather values from the form, build an object, and call the addMessage function with the object used as argument */
