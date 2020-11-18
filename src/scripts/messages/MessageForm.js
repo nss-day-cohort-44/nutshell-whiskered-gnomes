3e2d65604639a87aa25ed847881edb1bcd2d6b53
Kristen Steele <ksteelephoto@gmail.com>
2020-11-18 14:40:00 -0600

    add event listener for send button. check button id. pull form values.

--- src/scripts/messages/MessageForm.js
+++ src/scripts/messages/MessageForm.js
@@ -2,6 +2,8 @@
 then listens for the form button click, saves input values and uses them as 
 an argument to call the saveMessage function */
 
+const eventHub = document.querySelector(".container")
+
 /* MessageForm is called by Nutshell.
 This function defines a content target and adds to it the html to render a message form 
 for adding a new message to the public chat, consisting of an input field and a send button. */
@@ -14,4 +16,20 @@ export const MessageForm = () => {
 }
 
 /* This page needs an event listener to listen for the click of the send button,
-gather values from the form, build an object, and call the addMessage function with the object used as argument */
+gather values from the form, build an object, and call the 
+addMessage function with the object used as argument */
+eventHub.addEventListener("click", event => {
+    if (event.target.id === "saveMessage"){
+        console.log(event)
+        const userId = parseInt(sessionStorage.getItem("activeUser"))
+        const message = document.querySelector(".message__field").value 
+        const timestamp = event.timeStamp
+
+        const newMessageObj = {
+            userId,
+            message,
+            timestamp
+        }
+    }
+    console.log("new message: ", newMessageObj)
+})
