976a5a43035201bc3549540681896ab3448766e4
Kristen Steele <ksteelephoto@gmail.com>
2020-11-17 21:44:11 -0600

    add call and import for MessageList function to Nutshell function

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,6 +1,9 @@
+import { MessageList } from "./messages/MessageList.js"
+
 export const Nutshell = () => {
     // Render all your UI components here
-    const contentTarget = document.querySelector(".dashboard")
+    // const contentTarget = document.querySelector(".dashboard")
 
-    contentTarget.innerHTML = `<h1>Sometimes you feel like a nut</h1>`
+    // contentTarget.innerHTML = `<h1>Sometimes you feel like a nut</h1>`
+    MessageList()
 }
