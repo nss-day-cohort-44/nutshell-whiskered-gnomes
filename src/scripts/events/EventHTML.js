b976af105b07b314f720a8b6ec346d92fb8302d0
Danny McCracken <dannymccracken13@yahoo.com>
2020-11-19 13:16:34 -0600

    morse css styling

--- src/scripts/events/EventHTML.js
+++ src/scripts/events/EventHTML.js
@@ -1,13 +1,13 @@
 // Author: Danny- create a component reponsible for rendering an upcoming event as an HTML card 
 export const eventHTML = (eventObj) => {
     return `
-    <ul id="entry--${eventObj.id}" class="event__card ${eventObj.upcomingEvent ? `upcomingEvent`: ``}">
-        <li>User: ${eventObj.user.username}</li>
-        <li>Event: ${eventObj.eventName}</li>
-        <li>Date: ${eventObj.date}</li>
-        <li>Location: ${eventObj.location}</li>
+    <div id="entry--${eventObj.id}" class="event__card ${eventObj.upcomingEvent ? `upcomingEvent`: ``}">
+        <p>User: ${eventObj.user.username}</p>
+        <p>Event: ${eventObj.eventName}</p>
+        <p>Date: ${eventObj.date}</p>
+        <p>Location: ${eventObj.location}</p>
         <button id="deleteEvent--${eventObj.id}">Delete</button>
         <button id="weatherButton">Weather</button>
-    </ul>
+    </div>
     `;
 };
