eed75e633726110a25ecfbc95621d0d398da1ea4
Danny McCracken <dannymccracken13@yahoo.com>
2020-11-19 13:45:03 -0600

    Refactored eventhtml and acrticle html to fix delete button bug

--- src/scripts/articles/articleHTML.js
+++ src/scripts/articles/articleHTML.js
@@ -1,6 +1,7 @@
 //J.Kaset HTML representation of one article. 
 
 export const Article = (articleObj) => {
+  if(articleObj.userId === parseInt(sessionStorage.getItem("activeUser"))) {
   return `
     <div class="news--card" id="userId--${articleObj.user.id}">
     <p><a href="#" id="${articleObj.user.id}">${articleObj.user.username}</a>:
@@ -12,4 +13,16 @@ export const Article = (articleObj) => {
     <button id="deleteArticle--${articleObj.id}">Delete</button>
     </div>
     `
+  }
+  else {
+    return`
+    <div class="news--card" id="userId--${articleObj.user.id}">
+    <p><a href="#" id="${articleObj.user.id}">${articleObj.user.username}</a>:
+    <h4>${articleObj.title}</h4>
+
+    <p>Synopsis: ${articleObj.synopsis}</p>
+    <p>URL: ${articleObj.url}</p>
+    </div>
+    `
+    }
   }
--- src/scripts/events/EventHTML.js
+++ src/scripts/events/EventHTML.js
@@ -1,6 +1,7 @@
 // Author: Danny- create a component reponsible for rendering an upcoming event as an HTML card 
 export const eventHTML = (eventObj) => {
-    return `
+    if(eventObj.userId === parseInt(sessionStorage.getItem("activeUser"))) {
+        return `
     <ul id="entry--${eventObj.id}" class="event__card ${eventObj.upcomingEvent ? `upcomingEvent`: ``}">
         <li>User: ${eventObj.user.username}</li>
         <li>Event: ${eventObj.eventName}</li>
@@ -9,4 +10,15 @@ export const eventHTML = (eventObj) => {
         <button id="deleteEvent--${eventObj.id}">Delete</button>
     </ul>
     `;
+    }
+    else {
+        return`
+        <ul id="entry--${eventObj.id}" class="event__card ${eventObj.upcomingEvent ? `upcomingEvent`: ``}">
+        <li>User: ${eventObj.user.username}</li>
+        <li>Event: ${eventObj.eventName}</li>
+        <li>Date: ${eventObj.date}</li>
+        <li>Location: ${eventObj.location}</li>
+    </ul>
+    `; 
+    }
 };
